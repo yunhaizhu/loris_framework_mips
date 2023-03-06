9bd04f4a02aa2776bf202f192ee0df991a66b7a2
Yunhai Zhu <yunhaia2@gmail.com>
2023-03-06 09:34:38 +0800

    update init.sh and cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,13 +1,3 @@
-#unlink loris_framework/src/mod_vm
-#unlink loris_framework/src/mod_vm_arch
-#unlink loris_framework/src/mod_vm_arch_mips_cp0
-#unlink loris_framework/src/mod_vm_arch_mips_cpu
-#unlink loris_framework/src/mod_vm_device
-#unlink loris_framework/src/mod_vm_device_manager
-#unlink loris_framework/src/mod_vm_device_vtty
-#unlink loris_framework/src/mod_vm_memory
-#unlink loris_framework/files/vmlinux
-
 sudo umount loris_framework/src/mod_vm
 sudo umount loris_framework/src/mod_vm_arch
 sudo umount loris_framework/src/mod_vm_arch_mips_cp0
